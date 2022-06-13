73679eef0f5fa3bb31fc37ae4562e9d39f281011
Julia Borges <89038897+Juliapixel@users.noreply.github.com>
2022-06-13 00:54:50 -0300

    updated the cli arguments to use Command struct

--- src/main.rs
+++ src/main.rs
@@ -5,79 +5,96 @@ pub mod bitimage;
 
 use core::panic;
 use std::{env, path::Path};
-use clap::Parser;
-
-#[derive(Parser, Debug)]
-#[clap(author, version, about)]
-struct Args {
-  #[clap(short, long, help = "path to printer device")]
-  path_to_printer: String,
-
-  #[clap(short, long, help = "image file input")]
-  input: Option<String>,
-
-  #[clap(short, long, default_value_t = 128, help = "width in dots for images")]
-  width: u32,
-
-  #[clap(long, default_value_t = 7, help = "width used for qr codes, must be in range 0..16")]
-  qr_code_width: u8,
-
-  #[clap(short, long)]
-  qr_code: Option<String>,
-
-  #[clap(short, long)]
-  debug: bool,
-
-  #[clap(short, long)]
-  text: Option<String>,
-
-  #[clap(short, long, help = "must be either \"left\", \"center\" or \"right\", fallsback to \"left\"", default_value = "left")]
-  justification: String
-}
+use clap::{Parser, Arg};
 
 fn main() {
   env::set_var("RUST_BACKTRACE", "1");
 
-
+  let mut cmd = clap::Command::new("thermal_printer")
+    .arg(Arg::new("path_to_printer")
+      .short('p')
+      .long("path")
+      .required(true)
+      .help("path to the printer file handle, see documentation for help (todo)")
+    )
+    .arg(Arg::new("input")
+      .short('i')
+      .long("input")
+      .help("image file path")
+    )
+    .arg(Arg::new("width")
+      .short('w')
+      .long("width")
+      .default_value("128")
+      .help("width of the image in pixels")
+    )
+    .arg(Arg::new("qr_code_width")
+      .long("qr_width")
+      .help("width of the qr code, must be in range 0..16")
+    )
+    .arg(Arg::new("qr_code")
+      .short('q')
+      .long("qr_code")
+      .help("print a qr code with the given text encoded into it")
+    )
+    .arg(Arg::new("debug")
+     .short('d')
+     .long("debug")
+     .takes_value(false)
+    )
+    .arg(Arg::new("text")
+      .short('t')
+      .long("text")
+      .help("print the given text")
+    )
+    .arg(Arg::new("justification")
+      .short('j')
+      .long("justification")
+      .default_value("left")
+      .help("must be either \"left\", \"center\" or \"right\", falls back to \"left\"")
+    )
+  ;
   let cur_dir = env::current_dir().expect("error getting cwd!").to_str().expect("error turning path into string!").to_owned();
 
-  let args = Args::parse();
-  let mut printer = printing::Printer::new(args.path_to_printer.as_str());
+  let args = cmd.clone().get_matches();
+  let mut printer = printing::Printer::new(args.value_of("path_to_printer").unwrap());
 
-  match args.justification.to_lowercase().as_str() {
+  match args.value_of("justification").unwrap().to_lowercase().as_str() {
     "left" => printer.set_justification(0),
     "center" => printer.set_justification(1),
     "right" => printer.set_justification(2),
     _ => printer.set_justification(0)
   }
 
-  if let Some(path) = args.input {
+  if let Some(path) = args.value_of("input") {
     let image_path: String;
 
     if Path::new(&path).exists() {
-      image_path = path;
+      image_path = path.to_owned();
     } else if Path::new(&(cur_dir.clone() + &path)).exists() {
       image_path = cur_dir + &path
     } else {
       panic!("error finding file!");
     }
-    printer.print_image(image_path.as_str(), args.width);
+    printer.print_image(image_path.as_str(), args.value_of("width").unwrap().parse().unwrap());
 
     return
   }
 
-  if let Some(qr_code_text) = args.qr_code {
-    printer.print_qr_code(args.qr_code_width, qr_code_text.as_bytes());
-    if args.debug {
-      printer.println(qr_code_text.as_str());
+  if let Some(qr_code_text) = args.value_of("qr_code") {
+    printer.print_qr_code(args.value_of("qr_code_width").unwrap().parse().unwrap(), qr_code_text.as_bytes());
+    if args.is_present("debug") {
+      printer.println(qr_code_text);
     }
     return
   }
 
-  if let Some(text) = args.text {
-    printer.println(text.as_str());
+  if let Some(text) = args.value_of("text") {
+    printer.println(text);
     return
   }
+
+  cmd.print_help().expect("failed printing help!");
 
   // printer.print_qr_code(10, b"https://oisumida.rs/");
 
